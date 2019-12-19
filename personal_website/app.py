c58f0b6dbf0783e0b00671bb866b7b818291ef41
Octavian Stoch <stoch.octavian@gmail.com>
2019-12-19 14:01:24 -0600

    using bootstrap css and adding possible project titles to website

---
+++ personal_website/app.py
@@ -0,0 +1,36 @@
+from flask import Flask, redirect, url_for, request, render_template
+
+app = Flask(__name__)
+
+
+@app.route('/success/<name>')
+def success(name):
+   return 'welcome %s' % name
+
+@app.route('/login',methods = ['POST', 'GET'])
+def login():
+   if request.method == 'POST':
+      user = request.form['nm']
+      return redirect(url_for('success',name = user))
+   else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',name = user))
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/saystuff')
+def saystuff():
+   return render_template('saystuff.html')
+
+@app.route('/projects')
+def projects():
+   return render_template('projects.html')
+
+@app.route('/java')
+def javaPrograms():
+   return render_template('javaProblems.html')
+
+if __name__ == '__main__':
+   app.run(debug = True)
