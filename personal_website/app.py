90083cd30edeb3c2c94172496511b9b9720df75a
Octavian Stoch <stoch.octavian@gmail.com>
2019-12-20 16:03:20 -0600

    top navigation bar going to home/projects/contact

--- personal_website/app.py
+++ personal_website/app.py
@@ -32,5 +32,9 @@ def projects():
 def javaPrograms():
    return render_template('javaProblems.html')
 
+@app.route('/contact')
+def contact():
+   return render_template('contact.html')
+
 if __name__ == '__main__':
    app.run(debug = True)
